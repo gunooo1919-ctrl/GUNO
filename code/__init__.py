1ecf40f95e2f94287736a9d32648d57fd4f84cf5
gunooo1919-ctrl <gunooo1919@gmail.com>
2025-09-27 20:52:35 +0700

    Create __init__.py

---
+++ code/__init__.py
@@ -0,0 +1,25 @@
+"""
+DIKTEMAN - Root Package
+
+Repository: Edukasi Finansial & Revitalisasi Budaya
+
+Deskripsi:
+File ini merupakan inisialisasi package Python pada folder 'code'.
+Fungsinya agar folder 'code' dikenali sebagai package Python, sehingga modul-modul di dalamnya dapat diimpor ke notebook ataupun script lain.
+
+Isi yang direkomendasikan:
+- Metadata dasar package
+- Optional: import fungsi/kelas penting agar mudah diakses dari package utama
+- Optional: versi package
+
+Contoh isi:
+"""
+
+__version__ = "0.1.0"
+__author__ = "gunooo1919-ctrl"
+
+# Optional imports (misal: modul utama)
+# from .modul_simulasi import SimulasiKeuangan
+# from .modul_pembelajaran import MateriBudaya
+
+# Catatan: Tambahkan modul sesuai kebutuhan pengembangan
